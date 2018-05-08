9275f0d7e31d89321f59587c2f567b8a8e44a701
Tomas Rojo <tomasn4a@gmail.com>
2018-05-08 20:24:50 +0100

    fix bug in db_creator

--- db_creator.py
+++ db_creator.py
@@ -9,7 +9,7 @@ c.execute('''CREATE TABLE test_table
              (name text, age integer, r_float real)''')
 
 for i in range(10000):
-    c.execute("INSERT INTO test_table VALUES ('name_{}', {}, {})".format(i%10, random.randint(18, 80), random.uniform(0, 10))
+    c.execute("INSERT INTO test_table VALUES ('name_{}', {}, {})".format(i%10, random.randint(18, 80), random.uniform(0, 10)))
 
 conn.commit()
 
