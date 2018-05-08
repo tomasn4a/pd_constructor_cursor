9301f9e16400049c50228027316db18cd2b6334f
Tomas Rojo <tomasn4a@gmail.com>
2018-05-08 20:13:31 +0100

    modify database creator

--- db_creator.py
+++ db_creator.py
@@ -8,8 +8,8 @@ c = conn.cursor()
 c.execute('''CREATE TABLE test_table
              (name text, age integer, r_float real)''')
 
-for i in range(10):
-    c.execute("INSERT INTO test_table VALUES ('name_{}', {}, {})".format(i, i, random.randint(18, 80)))
+for i in range(10000):
+    c.execute("INSERT INTO test_table VALUES ('name_{}', {}, {})".format(i%10, random.randint(18, 80), random.uniform(0, 10))
 
 conn.commit()
 
